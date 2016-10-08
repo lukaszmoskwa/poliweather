1911fe076d0d46f02e832e91b6c46190606f514f
Lukasz Moskwa <lukaszmoskwa94@gmail.com>
2016-10-08 19:07:07 +0200

    Added to server.py the function for forecast request, not only weather

--- server/server.py
+++ server/server.py
@@ -29,6 +29,18 @@ def getcitybycoord(lat, lon):
 		return response.read()	
 	except urllib2.URLError as e:
 		return "{\"cod\": 500}"
+
+# TODO: Aggiungere la documentazione e i commenti in inglese
+# Prendere le informazioni di forecast (previsione nelle prossime ore fino a 5 giorni)
+
+@app.route("/city/3/<cityname>")
+def getcitybyname3(cityname):
+	cityname = cityname.replace(" ", "")
+	try:
+		response = urllib2.urlopen('http://api.openweathermap.org/data/2.5/forecast?lang=it&APPID=' + appkey + '&q=' + cityname)
+		return response.read()
+	except urllib2.URLError as e:
+		return "{\"cod\": 500" 
 	 
 
 if __name__ == "__main__":
